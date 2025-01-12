4db0ab9ca14482c6bd9a4c1384485e779659f584
Jacob <92279567+Oia20@users.noreply.github.com>
2025-01-12 16:36:47 -0600

    Added meta tags. Added Robot.txt, and favicon.

--- dement.dev/astro.config.mjs
+++ dement.dev/astro.config.mjs
@@ -5,7 +5,9 @@ import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), sitemap()]
 });
