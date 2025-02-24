2757ce6594ba64b20df6ebd365789e37cde14083
Jacob <92279567+Oia20@users.noreply.github.com>
2025-02-24 15:07:46 -0600

    Added contact section.

---
+++ rebuild/src/components/Contact.tsx
@@ -0,0 +1,17 @@
+export const Contact = () => {
+    return (
+        <div className="transition duration-500 ease-in-out dark:bg-zinc-900 bg-gray-50 flex flex-col items-start px-8 pt-2 sm:items-center sm:py-12">
+          <div className="max-w-2xl text-left justify-center">
+            <h1 className="text-xl font-bold text-gray-900 mb-2 transition-colors dark:text-zinc-50">
+              Connect
+            </h1>
+            
+            <p className="text-zinc-300 leading-relaxed max-w-prose">
+                If you want to get in touch with me about an inquiry, just want to say hi, or anything in between, shoot me a message on social media, or an email!
+            </p>
+          </div>
+        </div>
+    );
+  };
+  
+  
