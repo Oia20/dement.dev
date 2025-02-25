68602c541b8198b035ae9218bc29313805efe938
Jacob <92279567+Oia20@users.noreply.github.com>
2025-02-24 21:04:29 -0600

    Added social svg's to contact section.

--- rebuild/src/components/Contact.tsx
+++ rebuild/src/components/Contact.tsx
@@ -1,17 +1,30 @@
 export const Contact = () => {
     return (
         <div className="transition duration-500 ease-in-out dark:bg-zinc-900 bg-gray-50 flex flex-col items-start px-8 pt-2 sm:items-center sm:py-12">
-          <div className="max-w-2xl text-left justify-center">
+          <div className="max-w-2xl text-left">
             <h1 className="text-xl font-bold text-gray-900 mb-2 transition-colors dark:text-zinc-50">
-              Connect
+              Get in touch
             </h1>
             
-            <p className="text-zinc-300 leading-relaxed max-w-prose">
-                If you want to get in touch with me about an inquiry, just want to say hi, or anything in between, shoot me a message on social media, or an email!
+            <p className="text-zinc-600 dark:text-zinc-300 leading-relaxed max-w-prose">
+                I would love to hear from you! Whether you have an inquiry or just want to say hi, feel free to reach out to me on social media or shoot me an email.
             </p>
           </div>
+          <div className="flex flex-row items-center ml-[-1.5vw] mt-8 space-x-4 w-full max-w-prose">
+            <a href="https://www.linkedin.com/in/jacob-dement-35658b275/" className="text-zinc-600 dark:text-zinc-300 hover:text-zinc-400 transition-colors">
+            <svg width="48px" height="48px" viewBox="0 0 48 48" version="1.1" xmlns="http://www.w3.org/2000/svg" className="social-icon fill-current">
+                <g id="Icon/Social/linkedin-color" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                    <path d="M20.9716667,33.5527338 L25.001,33.5527338 L25.001,27.1328007 C25.001,25.439485 25.3213333,23.7988354 27.4206667,23.7988354 C29.491,23.7988354 29.517,25.7351486 29.517,27.2404662 L29.517,33.5527338 L33.5506667,33.5527338 L33.5506667,26.4341413 C33.5506667,22.9381777 32.796,20.2505391 28.711,20.2505391 C26.7483333,20.2505391 25.432,21.3265278 24.8943333,22.3471839 L24.839,22.3471839 L24.839,20.5725357 L20.9716667,20.5725357 L20.9716667,33.5527338 Z M16.423,14.1202696 C15.1273333,14.1202696 14.0823333,15.1682587 14.0823333,16.4595785 C14.0823333,17.7508984 15.1273333,18.7992208 16.423,18.7992208 C17.7133333,18.7992208 18.761,17.7508984 18.761,16.4595785 C18.761,15.1682587 17.7133333,14.1202696 16.423,14.1202696 L16.423,14.1202696 Z M14.4026667,33.5527338 L18.4406667,33.5527338 L18.4406667,20.5725357 L14.4026667,20.5725357 L14.4026667,33.5527338 Z M9.76633333,40 C8.79033333,40 8,39.2090082 8,38.2336851 L8,9.76631493 C8,8.79065843 8.79033333,8 9.76633333,8 L38.234,8 C39.2093333,8 40,8.79065843 40,9.76631493 L40,38.2336851 C40,39.2090082 39.2093333,40 38.234,40 L9.76633333,40 Z" id="Shape" className="fill-current"></path>
+                </g>
+            </svg>
+            </a>
+            <a href="https://github.com/oia20" className="text-zinc-600 dark:text-zinc-300 hover:text-zinc-400 transition-colors">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="social-icon fill-current"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" className="fill-current"></path></svg>
+            </a>
+          </div>
+          <p className="text-zinc-600 dark:text-zinc-300 w-full max-w-prose">jacob.dement2002@gmail.com</p>
+
         </div>
     );
-  };
-  
-  
+};
+
