168276456f99179a2e0c531a287779246351f29d
Jacob <92279567+Oia20@users.noreply.github.com>
2025-02-26 16:52:42 -0600

    Deleted original dement.dev and replaced with completed rebuild.

--- dement.dev/astro.config.mjs
+++ dement.dev/astro.config.mjs
@@ -3,11 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
-import tailwind from '@astrojs/tailwind';
-
-import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), sitemap()]
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
