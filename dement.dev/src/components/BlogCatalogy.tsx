40a17225419cb89b9a8d032e019e2256fdd57e19
Jacob <92279567+Oia20@users.noreply.github.com>
2025-03-04 17:22:57 -0600

    Added blog page + route

---
+++ dement.dev/src/components/BlogCatalogy.tsx
@@ -0,0 +1,108 @@
+
+export const BlogCatalogy = () => {
+
+    return (
+        <div id="blog-catalogy" className="transition duration-500 ease-in-out dark:bg-zinc-900 bg-gray-50 flex flex-col items-start px-8 pt-2 sm:items-center py-12">
+            <div className="max-w-2xl text-left justify-center pb-4 border-b border-gray-200 dark:border-zinc-700">
+                <h1 className="text-2xl font-bold text-gray-900 mb-2 transition-colors dark:text-zinc-50">
+                    Blog
+                </h1>
+                <p className="dark:text-zinc-300 text-gray-600 leading-relaxed max-w-prose italic">
+                    Welcome to my blog! This is still under development, but in the future I will write blog posts here!
+                </p>
+            </div>
+            <div className="flex flex-col items-start gap-4 mt-4">
+
+                <div className="flex flex-row items-center gap-1 group text-zinc-900 dark:text-zinc-100 hover:text-zinc-400 transition-colors duration-200 hover:cursor-pointer"
+                onClick={() => window.location.href = "https://formbee.dev/blog"}
+                >
+                {/* <p
+                className=""
+                rel="noopener noreferrer"
+                >                
+                View All
+                </p> */}
+                <div className="relative w-4 h-4">
+                    <div className="arrow-container">
+                    {/* Left arrow */}
+                        {/* <svg
+                        viewBox="0 0 300 300"
+                        shapeRendering="geometricPrecision"
+                        textRendering="geometricPrecision"
+                        className="arrow arrow-left"
+                        >
+                        <g transform="translate(100 -.000001)">
+                            <rect
+                            width="141.795482"
+                            height="10.701546"
+                            transform="matrix(.750432 0.660948-.660948 0.750432 100.332654 44.266147)"
+                            fill="currentColor"
+
+
+                            />
+                            <rect
+                            width="141.795482"
+                            height="10.701546"
+                            transform="matrix(.687963-.781105 0.750432 0.660948 94.987956 248.660691)"
+                            fill="currentColor"
+
+
+                            />
+                        </g>
+                        </svg>
+
+                        {/* Center arrow */}
+                        {/* <svg
+                        viewBox="0 0 300 300"
+                        shapeRendering="geometricPrecision"
+                        textRendering="geometricPrecision"
+                        className="arrow arrow-center"
+                        >
+                        <g transform="translate(.000001 0)">
+                            <rect
+                            width="141.795482"
+                            height="10.701546"
+                            transform="matrix(.750432 0.660948-.660948 0.750432 100.332654 44.266147)"
+                            fill="currentColor"
+                            />
+                            <rect
+                            width="141.795482"
+                            height="10.701546"
+                            transform="matrix(.687963-.781105 0.750432 0.660948 94.987956 248.660691)"
+                            fill="currentColor"
+
+                            />
+                        </g>
+                        </svg>
+
+                        {/* Right arrow */}
+                        {/* <svg
+                        viewBox="0 0 300 300"
+                        shapeRendering="geometricPrecision"
+                        textRendering="geometricPrecision"
+                        className="arrow arrow-right"
+                        >
+                        <g transform="translate(-135.000501 0)">
+                            <rect
+                            width="141.795482"
+                            height="10.701546"
+                            transform="matrix(.750432 0.660948-.660948 0.750432 137.619946 44.266147)"
+                            fill="currentColor"
+
+                            />
+                            <rect
+                            width="141.795482"
+                            height="10.701546"
+                            transform="matrix(.687963-.781105 0.750432 0.660948 132.275248 248.660691)"
+                            fill="currentColor"
+
+                            />
+                        </g>
+                        </svg>  */}
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    );
+}
