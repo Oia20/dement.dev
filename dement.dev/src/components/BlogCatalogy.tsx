91b6cd0a50d12fb2b39f1e2e334bdb42de7b372f
Jacob <92279567+Oia20@users.noreply.github.com>
2025-03-07 16:40:24 -0600

    implementing view tracker.

--- dement.dev/src/components/BlogCatalogy.tsx
+++ dement.dev/src/components/BlogCatalogy.tsx
@@ -1,20 +1,51 @@
-
+import { useEffect, useState } from "react";
 
 export const BlogCatalogy = () => {
+
+    const [posts, setPosts] = useState<Post[]>(
+        [
+            {
+                title: "Railway Functions - The fastest way to deploy your serverless functions",
+                description: "A look at how to use Railway Functions in your project",
+                date: "03/07/2025",
+                path: "/blog/railway",
+                viewCountURL: "https://function-bun-production-cbf5.up.railway.app/",
+                viewCount: 0,
+            },
+        ]
+    );
+
+
     type Post = {
         title: string;
         description: string;
         date: string;
         path: string;
+        viewCountURL: string;
+        viewCount: number;
     }
-    const posts = [
-        {
-            title: "Railway Functions - The fastest way to deploy your serverless functions",
-            description: "A look at how to use Railway Functions in your project",
-            date: "2023-01-01",
-            path: "/blog/railway"
-        },
-    ]
+
+    useEffect(() => {
+        for (const post of posts) {
+            fetch(post.viewCountURL)
+                .then((response) => response.json())
+                .then((data) => {
+                    setPosts((prevPosts) => {
+                        const updatedPosts = prevPosts.map((prevPost) => {
+                            console.log(prevPost.path);
+                            if (prevPost.path === post.path) {
+                                return {
+                                    ...prevPost,
+                                    viewCount: data.count,
+                                };
+                            }
+                            return prevPost;
+                        });
+                        return updatedPosts;
+                    });
+                });
+        }
+    }, []);
 
 
     return (
@@ -40,10 +71,20 @@ export const BlogCatalogy = () => {
                                 {post.description}
                             </p>
                         </div>
-                        <div className="flex items-center gap-2">
+                        <div className="w-full flex items-center justify-between">
                             <p className="text-sm text-gray-500 dark:text-zinc-400">
                                 {post.date}
                             </p>
+                            <div className="flex items-center gap-1 text-gray-500 dark:text-zinc-400">
+                                <svg width="16px" height="16px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-eye">
+                                    <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+                                    <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+                                </svg>
+
+                                <p className="text-sm text-gray-500 dark:text-zinc-400">
+                                    {post.viewCount} 
+                                </p>
+                            </div>
                         </div>
                     </div>
                 ))}
