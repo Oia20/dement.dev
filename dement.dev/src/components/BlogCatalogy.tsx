3693d0c9d67a016050379105386aa24823f34445
Jacob <92279567+Oia20@users.noreply.github.com>
2025-03-11 16:48:34 -0500

    Added loading state to view count

--- dement.dev/src/components/BlogCatalogy.tsx
+++ dement.dev/src/components/BlogCatalogy.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 
 export const BlogCatalogy = () => {
 
+    const [viewCountLoaded, setViewCountLoaded] = useState(false);
+
     const [posts, setPosts] = useState<Post[]>(
         [
             {
@@ -47,6 +49,7 @@ export const BlogCatalogy = () => {
                             }
                             return prevPost;
                         });
+                        setViewCountLoaded(true);
                         return updatedPosts;
                     });
                 });
@@ -107,10 +110,13 @@ export const BlogCatalogy = () => {
                                     <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
                                     <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
                                 </svg>
-
-                                <p className="text-sm text-gray-500 dark:text-zinc-400">
-                                    {post.viewCount} 
-                                </p>
+                                {!viewCountLoaded ? (
+                                    <div className="w-4 h-4 border-2 border-gray-300 border-t-transparent rounded-full animate-spin"></div>
+                                    ) : (
+                                    <p className="text-sm text-gray-500 dark:text-zinc-400">
+                                        {post.viewCount}
+                                    </p>
+                                )}
                             </div>
                         </div>
                     </div>
